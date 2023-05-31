29081c7c88a1c629f585ee4b2fedabd64c9dd298
shwetank08 <shwetanksingh.in@gmail.com>
2023-05-31 22:06:48 +0530

    working on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,65 +1,107 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
-import Badge from 'react-bootstrap/Badge';
-import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroup from "react-bootstrap/ListGroup";
+import Accordion from "react-bootstrap/Accordion";
 
 const Home = () => {
   const [task, setTask] = useState({
     title: "",
-    description: ""
+    description: "",
   });
-  const display = () => {
-    return(
-    {for (const [key, value] of Object.entries(localStorage)) {
-      return(
-      <ListGroup as="ol" numbered>
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">{key}</div>
-          {value}
-        </div>
-      </ListGroup.Item>
-    </ListGroup>)
-   }} 
-  }
 
-  const handleClick = (e) => {
+  const [items, setItems] = useState([
+    {
+      title: "",
+      description: "",
+    },
+  ]);
+  const display = () => {
+    {
+      items &&
+        items.map((e, i) => {
+          return (
+            <Accordion>
+              <Accordion.Item eventKey={i}>
+                <Accordion.Header>{e.title}</Accordion.Header>
+                <Accordion.Body>{e.description}</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          );
+        });
+    }
+  };
+
+  const handleClick = async (e) => {
     e.preventDefault();
-    console.log(task);
+    setItems("");
     localStorage.setItem(task.title, task.description);
+    for (const [key, value] of Object.entries(localStorage)) {
+      setItems((list) => [
+        ...list,
+        {
+          title: key,
+          description: value,
+        },
+      ]);
+    }
+    console.log(items);
     setTask("");
     display();
-  }
+  };
+
+  
 
   return (
-    <Container style={{
-      marginTop: "5rem",
-      marginBottom: "5rem",
-    }}
-    className="flex flex-wrap gap-3">
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Title</Form.Label>
-        <Form.Control type="text" placeholder="Title Here"  value={task.title ? task.title : ""} onChange={(e)=>{setTask({...task, title: e.target.value})}}/>
-      </Form.Group>
+    <Container
+      style={{
+        marginTop: "5rem",
+        marginBottom: "5rem",
+      }}
+      className="flex flex-wrap gap-3"
+    >
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Title Here"
+            value={task.title ? task.title : ""}
+            onChange={(e) => {
+              setTask({ ...task, title: e.target.value });
+            }}
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Description</Form.Label>
-        <Form.Control type="text" placeholder="Describe Your Task" value={task.description ? task.description : ""} onChange={(e)=>{setTask({...task, description: e.target.value})}}/>
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleClick}>
-        Add Task
-      </Button>
-    </Form>
-
-
-
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Describe Your Task"
+            value={task.description ? task.description : ""}
+            onChange={(e) => {
+              setTask({ ...task, description: e.target.value });
+            }}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleClick}>
+          Add Task
+        </Button>
+        <ListGroup className="mt-4">
+          {items &&
+            items.map((e,i) => {
+              return (
+                <Accordion>
+                  <Accordion.Item eventKey={i}>
+                    <Accordion.Header>{e.title}</Accordion.Header>
+                    <Accordion.Body>{e.description}</Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              );
+            })}
+        </ListGroup>
+      </Form>
     </Container>
   );
 };
